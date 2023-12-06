49cbd0d797d5296f38c30d2fcaa69717e41405b9
Yoel Buzgalo <yoelbuzgalo@gmail.com>
2023-12-06 10:31:46 -0500

    Mini-practicum (Part 2)

--- practice.py
+++ practice.py
@@ -1,5 +1,6 @@
 from node_stack import Stack
 from array_queue import Queue
+import random
 
 def reverse_digits(a_digit):
     strigified_digit = str(a_digit)
@@ -14,8 +15,29 @@ def reverse_digits(a_digit):
     
     return int(new_string_digit)
 
+def hot_potato(a_list, rounds):
+    queue = Queue()
+    
+    for name in a_list:
+        queue.enqueue(name)
+    
+    for _ in range(rounds):
+        random_selected = random.randrange(0, len(a_list))
+        potato_holder = queue.front()
+        for _ in range(random_selected):
+            person = queue.dequeue()
+            queue.enqueue(person)
+        receiver = queue.back()
+        print(potato_holder, "tosses the potato to", receiver)
+    
+    print("The music stops and", queue.back(), "is holding the potato!")
+
+        
+
+
 def main():
     print(reverse_digits(14))
+    hot_potato(['Bobby', 'Bruce', 'Chris', 'Dave'], 4)
 
 if __name__ == "__main__":
     main()

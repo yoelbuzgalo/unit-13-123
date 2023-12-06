29b3d3539d74cac700b74fe34d17b2752e8be13c
Yoel Buzgalo <yoelbuzgalo@gmail.com>
2023-12-06 11:46:35 -0500

    Activity 13.4

--- review.py
+++ review.py
@@ -7,8 +7,20 @@ def zip_lookup(filename, zipcode):
             if zipcode == record[0]:
                 return record[1]
         raise ValueError("Doesn't exist in the file")
+    
+def is_power(a, b):
+    if a == 1:
+        return True
+    elif a % b == 0:
+        return is_power(a/b, b)
+    else:
+        return False
+
 
 def main():
-    print(zip_lookup("./data/zip_codes.csv", "00603"))
+    # print(zip_lookup("./data/zip_codes.csv", "00603"))
+    print(is_power(27, 3))
+
+
 if __name__ == "__main__":
     main()

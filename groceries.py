fae62c0f3fbfc678d697d17ce53007e9338e0f80
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-12-08 10:44:50 -0500

    Activity 13.20

--- groceries.py
+++ groceries.py
@@ -1,3 +1,5 @@
+import csv
+
 class GroceryItem:
     __slots__ = ['__name', '__weight', '__price']
 
@@ -34,18 +36,33 @@ class GroceryItem:
     
     def get_price(self):
         return self.__price
+
+def stock_store(filename):
+    stock = dict()
+    try:
+        with open(filename) as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)
+            for record in csv_reader:
+                stock[record[0]] = GroceryItem(record[0], record[1], record[2])
+        return stock
+    except FileNotFoundError:
+        print("There was an error trying to open the file")
+    return
     
 def main():
-    item_1 = GroceryItem('Banana', 4.08, 1.00)
-    item_2 = GroceryItem('Salami', 3.05, 1.35)
-    item_3 = GroceryItem('Bread', 1, 3)
-    item_4 = GroceryItem('Coke', 5.00, 1)
-    item_5 = GroceryItem('Wine', 2, 5)
+    # item_1 = GroceryItem('Banana', 4.08, 1.00)
+    # item_2 = GroceryItem('Salami', 3.05, 1.35)
+    # item_3 = GroceryItem('Bread', 1, 3)
+    # item_4 = GroceryItem('Coke', 5.00, 1)
+    # item_5 = GroceryItem('Wine', 2, 5)
     
-    items = [item_1, item_2, item_3, item_4, item_5]
+    # items = [item_1, item_2, item_3, item_4, item_5]
 
-    print(items)
-    print(sorted(items))
+    # print(items)
+    # print(sorted(items))
+
+    print(stock_store("./data/groceries.csv"))
 
 
 if __name__ == "__main__":

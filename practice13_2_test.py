5d74570c735c23612252cc39f53e543734e0c134
Yoel Buzgalo <yoelbuzgalo@gmail.com>
2023-12-11 02:25:11 -0500

    Assignment 13.2 (More deep testing)

--- practice13_2_test.py
+++ practice13_2_test.py
@@ -13,3 +13,49 @@ def test_street_initialization():
     assert expected_name == result.get_name()
     assert expected_type in result.get_types()
     assert expected_direction == result.get_direction()
+
+def test_street_types():
+    # Setup
+    first_type = 'FIRST'
+    second_type = 'SECOND'
+    street = practice13_2.Street('ABC', 'DIRECTION', first_type)
+
+    # Invoke
+    street.add_type(second_type)
+
+    assert len(street.get_types()) == 2
+    assert first_type in street.get_types()
+    assert second_type in street.get_types()
+
+def test_street_inquiry():
+    # Setup
+    street_1 = practice13_2.Street('ABC', 'DIRECTION', 'DR')
+    street_2 = practice13_2.Street('DFG', 'DIRECTION_2', 'LN')
+
+    # Invoke
+    streets = practice13_2.Streets()
+    streets.add_street(street_1)
+    streets.add_street(street_2)
+    result = streets.inquire_street('ABC', 'DR')
+    fail_result = streets.inquire_street('DFG', 'CT')
+
+
+    # Analysis
+    assert result == True
+    assert fail_result == False
+
+
+def test_streets_street_types():
+    # Setup
+    street_1 = practice13_2.Street('ABC', 'DIRECTION', 'DR')
+    street_2 = practice13_2.Street('DFG', 'DIRECTION_2', 'LN')
+    street_1.add_type('CT')
+
+    # Invoke
+    streets = practice13_2.Streets()
+    streets.add_street(street_1)
+    streets.add_street(street_2)
+
+    # Analysis
+    assert len(streets.get_street_types('ABC')) == 2
+    assert len(streets.get_street_types('DFG')) == 1
